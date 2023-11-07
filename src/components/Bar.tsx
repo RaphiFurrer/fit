e7c68c7033e52cdaebb8e9139529f66f5c3d53cc
tico <tico@mail.de>
2023-11-07 19:31:39 +0100

    ui improvements

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,34 +1,32 @@
-const Bar = ({ percentage }: {percentage: number}) => {
-    const barWidth = 300; // Width of the progress bar
-    const barHeight = 30; // Height of the progress bar
-    const borderStrokeWidth = 2; // Width of the border
-    const normalizedWidth = barWidth - borderStrokeWidth * 2; // Adjust for the border
-    const fillWidth = (percentage / 100) * normalizedWidth;
+const Bar = ({ percentage }: { percentage: number }) => {
+  const barWidth = 300; // Width of the progress bar
+  const barHeight = 30; // Height of the progress bar
+  const borderStrokeWidth = 2; // Width of the border
+  const normalizedWidth = barWidth - borderStrokeWidth * 2; // Adjust for the border
+  const fillWidth = (percentage / 100) * normalizedWidth;
 
-    return (
-        <div className="w-full mx-auto">
-            {/* Border Bar */}
-            <div
-                className="bg-gray-300"
-                style={{
-                    width: barWidth + 'px',
-                    height: barHeight + 'px',
-                }}
-            >
-                {/* Fill Bar */}
-                <div
-                    className="bg-[#00008f]"
-                    style={{
-                        width: fillWidth + 'px',
-                        height: barHeight + 'px',
-                    }}
-                ></div>
-            </div>
-            <div className="text-teal text-center mt-2">
-                {percentage}%
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full mx-auto">
+      {/* Border Bar */}
+      <div
+        className="bg-gray-300"
+        style={{
+          width: barWidth + 'px',
+          height: barHeight + 'px',
+        }}
+      >
+        {/* Fill Bar */}
+        <div
+          className="bg-[#00008f]"
+          style={{
+            width: fillWidth + 'px',
+            height: barHeight + 'px',
+          }}
+        ></div>
+      </div>
+      <div className="text-teal text-center mt-2">{percentage}%</div>
+    </div>
+  );
 };
 
 export default Bar;
--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,45 +1,45 @@
-const Circle = ({percentage}: { percentage: number }) => {
-    const radius = 150; // Radius of the circle
-    const strokeWidth = 30; // Width of the progress bar
-    const normalizedRadius = radius - strokeWidth * 2;
-    const circumference = normalizedRadius * 2 * Math.PI;
-    const strokeDashoffset = circumference - (percentage / 100) * circumference;
+const Circle = ({ percentage }: { percentage: number }) => {
+  const radius = 150; // Radius of the circle
+  const strokeWidth = 30; // Width of the progress bar
+  const normalizedRadius = radius - strokeWidth * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const strokeDashoffset = circumference - (percentage / 100) * circumference;
 
-    return (
-        <>
-            <svg height={radius * 2} width={radius * 2} className="mx-auto">
-                <circle
-                    fill="transparent"
-                    stroke="lightgray"
-                    strokeWidth={strokeWidth}
-                    r={normalizedRadius}
-                    cx={radius}
-                    cy={radius}
-                />
-                <circle
-                    fill="transparent"
-                    stroke="#00008f"
-                    strokeWidth={strokeWidth}
-                    strokeDasharray={circumference + ' ' + circumference}
-                    style={{strokeDashoffset}}
-                    r={normalizedRadius}
-                    cx={radius}
-                    cy={radius}
-                    transform={`rotate(-90 ${radius} ${radius})`} // Rotate the progress 90 degrees counterclockwise
-                />
-                <text
-                    x={radius}
-                    y={radius}
-                    fontSize="18"
-                    className="text-teal text-center"
-                    dominantBaseline="middle"
-                    textAnchor="middle"
-                >
-                    {percentage}%
-                </text>
-            </svg>
-        </>
-    )
-}
+  return (
+    <>
+      <svg height={radius * 2} width={radius * 2} className="mx-auto">
+        <circle
+          fill="transparent"
+          stroke="lightgray"
+          strokeWidth={strokeWidth}
+          r={normalizedRadius}
+          cx={radius}
+          cy={radius}
+        />
+        <circle
+          fill="transparent"
+          stroke="#00008f"
+          strokeWidth={strokeWidth}
+          strokeDasharray={circumference + ' ' + circumference}
+          style={{ strokeDashoffset }}
+          r={normalizedRadius}
+          cx={radius}
+          cy={radius}
+          transform={`rotate(-90 ${radius} ${radius})`} // Rotate the progress 90 degrees counterclockwise
+        />
+        <text
+          x={radius}
+          y={radius}
+          fontSize="18"
+          className="text-teal text-center"
+          dominantBaseline="middle"
+          textAnchor="middle"
+        >
+          {percentage}%
+        </text>
+      </svg>
+    </>
+  );
+};
 
 export default Circle;
