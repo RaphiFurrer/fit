8ee4161a89cbab1ddd0000378d75dc8c9744a448
RaphiFurrer <raphi.furrer@gmail.com>
2023-11-08 10:44:39 +0100

    social styles

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,9 +1,9 @@
-import social from '../assets/social.png'
+import social from '../assets/social.png';
 const Social = () => {
   return (
     <div className="social">
-Teile Deinen Erfolg mit deinen Freuden
-        <img src={social}/>
+      Teile Deinen Erfolg mit deinen Freuden
+      <img src={social} className="mt-4" />
     </div>
   );
 };
