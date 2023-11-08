3ef5c55b7540eb948c737ae51ad146d29cbe1849
RaphiFurrer <raphi.furrer@gmail.com>
2023-11-08 10:27:51 +0100

    day switch buttons

--- src/App.tsx
+++ src/App.tsx
@@ -141,13 +141,13 @@ function App() {
     <>
       <Login />
       <Link
-        className="absolute right-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        className="absolute right-1 top-1/3 text-gray-800 font-semibold py-2 px-4 scale-150"
         to={nextDay}
       >
         {'>'}
       </Link>
       <Link
-        className="absolute left-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        className="absolute left-1 top-1/3 text-gray-800 font-semibold py-2 px-4 scale-150"
         to={lastDay}
       >
         {'<'}
