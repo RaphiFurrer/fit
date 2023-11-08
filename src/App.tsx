e2268bcedea3a9e6e504538dafc255741f801d05
RaphiFurrer <raphi.furrer@gmail.com>
2023-11-08 10:04:19 +0100

    make day switch buttons absolute

--- src/App.tsx
+++ src/App.tsx
@@ -127,13 +127,13 @@ function App() {
     <>
       <Login />
       <Link
-        className="fixed right-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        className="absolute right-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
         to={nextDay}
       >
         {'>'}
       </Link>
       <Link
-        className="fixed left-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        className="absolute left-1 top-1/3 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
         to={lastDay}
       >
         {'<'}
